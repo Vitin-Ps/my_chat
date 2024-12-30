0a922214c3da5e7dce531d9325b0d843422c7a34
Vitin-Ps <vicssoares2802@gmail.com>
2024-12-30 00:11:48 -0300

    ..

--- src/interfaces/Grupo.ts
+++
@@ -1,5 +0,0 @@
-export interface Grupo {
-  id?: number;
-  uuid: string;
-  nome: string;
-}
--- src/interfaces/Usuario.ts
+++
@@ -1,7 +0,0 @@
-export interface Usuario {
-  id?: number;
-  login?: string;
-  senha?: string;
-  nome: string;
-  imagem?: string;
-}
